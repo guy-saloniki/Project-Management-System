cf6215ef03546fe4575b88f514cef3fe720a10a0
guy-saloniki <guysalonik@walla.com>
2023-08-23 20:29:10 +0300

    Bug fix 3

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
   );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
 }
 
 app.listen(port, () => {
